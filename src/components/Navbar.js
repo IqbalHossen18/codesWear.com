434d99fd5c635fcfb5ca6795fe8d03bfce45cf5d
Iqbal <iqbalhossen1418@gmail.com>
2023-11-19 23:51:31 +0600

    after all first setup..

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,191 +1,201 @@
 import Image from 'next/image'
-import Link from 'next/link'
-import { useRef } from 'react';
-import { FaShoppingCart, FaWindowClose, FaBars  } from "react-icons/fa";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useRef, useState } from 'react';
+import { FaShoppingCart, FaWindowClose, FaBars } from "react-icons/fa";
 import { FaCirclePlus, FaCircleMinus } from "react-icons/fa6";
 import { IoBagCheckOutline } from "react-icons/io5";
 
 const Navbar = () => {
-  const ref = useRef()
-  const cartref = useRef()
-  const togglebar=()=>{
-     if(ref.current.classList.contains('displaynone')){
-      ref.current.classList.remove('displaynone')
-      ref.current.classList.add('displayvisible')
-     }
-     else if(!ref.current.classList.contains('displaynone')){
-      ref.current.classList.remove('displayvisible')
-      ref.current.classList.add('displaynone')
-     }
-  }
-  const toggleCartBar = () => {
-    const cartClassList = cartref.current.classList;
-  
-    if (cartClassList.contains('top-[-110vh]')) {
-      cartClassList.remove('top-[-110vh]');
-      cartClassList.add('top-0');
-    } else {
-      // Remove the extra space in the class name
-      cartClassList.remove('top-0');
-      cartClassList.add('top-[-110vh]');
+    let pathname = usePathname()
+    const [menuonoff, setmenuonoff] = useState(true)
+    const ref = useRef()
+    const cartref = useRef()
+    const togglebar = () => {
+        setmenuonoff(!menuonoff)
+        if (ref.current.classList.contains('displaynone')) {
+            ref.current.classList.remove('displaynone')
+            ref.current.classList.add('displayvisible')
+        }
+        else if (!ref.current.classList.contains('displaynone')) {
+            ref.current.classList.remove('displayvisible')
+            ref.current.classList.add('displaynone')
+        }
     }
-  };
-  
-  return (
-    <>
-      <div id='nav'  className='p-2'>
-        <div className="logo-div">
-          <Link className='cursor-pointer' href={'/'}><Image src='/logo.png' alt='img' height={250} width={150} /></Link>
-        </div>
-        <div ref={ref} className=" navList displaynone">
-          <ul>
-            <Link onClick={togglebar} className='cursor-pointer' href={'/tshirt'}><li>Tshirts</li></Link>
-            <Link onClick={togglebar} className='cursor-pointer' href={'/hoodies'}><li>Hoodeis</li></Link>
-            <Link onClick={togglebar} className='cursor-pointer' href={'/mugs'}><li>Mugs</li></Link>
-            <Link onClick={togglebar} className='cursor-pointer' href={'/stickers'}><li>Stickers</li></Link>
-            <Link onClick={togglebar} className='cursor-pointer' href={'/about'}><li>About</li></Link>
-            <Link onClick={togglebar} className='cursor-pointer' href={'/contact'}><li>Contact</li></Link>
-            <Link onClick={togglebar} className='cursor-pointer' href={'/order'}><li>Order</li></Link>
-          </ul>
-        </div>
-        <div onClick={togglebar} className='menubar'>
-        <FaBars />
-        </div>
-      </div>
-      <div ref={cartref} className=" top-[-110vh] cartbar">
-          <div className="carthead">
-            <h2>Shopping Cart</h2>
-            <label htmlFor="carticon">
-             <FaShoppingCart onClick={toggleCartBar} className='cartopen'/>
-             <FaWindowClose onClick={toggleCartBar} className='cartclose'/>
-            </label>
-          </div>
-                        <div className="cartitem">
-                           <div className="cartlist">
-                                <div className="cartimg">
-                                    <img src='tshirts.jpg' alt='tshirt'/>
-                                </div>
-                                <div className="carttext">
-                                    <span>
-                                    Wings Of Freedom Attack On Titan Tshirt (XL/Bottlegreen)
-                                    </span>
-                                </div>
-                                <div className="cartstate">
-                                    <FaCircleMinus/><span>1</span><FaCirclePlus/>
-                                </div>
-                           </div>
-                           <div className="cartlist">
-                                <div className="cartimg">
-                                    <img src='tshirts.jpg' alt='tshirt'/>
-                                </div>
-                                <div className="carttext">
-                                    <span>
-                                    Wings  (XL/Bottlegreen)
-                                    </span>
-                                </div>
-                                <div className="cartstate">
-                                    <FaCircleMinus/><span>1</span><FaCirclePlus/>
-                                </div>
-                           </div>
-                           <div className="cartlist">
-                                <div className="cartimg">
-                                    <img src='tshirts.jpg' alt='tshirt'/>
-                                </div>
-                                <div className="carttext">
-                                    <span>
-                                    Wings Of Freedom Attack (XL/Bottlegreen)
-                                    </span>
-                                </div>
-                                <div className="cartstate">
-                                    <FaCircleMinus/><span>1</span><FaCirclePlus/>
-                                </div>
-                           </div>
-                           <div className="cartlist">
-                                <div className="cartimg">
-                                    <img src='tshirts.jpg' alt='tshirt'/>
-                                </div>
-                                <div className="carttext">
-                                    <span>
-                                    Wings Of Freedom Attack (XL/Bottlegreen)
-                                    </span>
-                                </div>
-                                <div className="cartstate">
-                                    <FaCircleMinus/><span>1</span><FaCirclePlus/>
-                                </div>
-                           </div>
-                           <div className="cartlist">
-                                <div className="cartimg">
-                                    <img src='tshirts.jpg' alt='tshirt'/>
-                                </div>
-                                <div className="carttext">
-                                    <span>
-                                    Wings Of Freedom Attack On Titan Tshirt (XL/Bottlegreen)
-                                    </span>
-                                </div>
-                                <div className="cartstate">
-                                    <FaCircleMinus/><span>1</span><FaCirclePlus/>
-                                </div>
-                           </div>
-                           <div className="cartlist">
-                                <div className="cartimg">
-                                    <img src='tshirts.jpg' alt='tshirt'/>
-                                </div>
-                                <div className="carttext">
-                                    <span>
-                                    Wings Of Freedom Attack On Titan Tshirt (XL/Bottlegreen)
-                                    </span>
-                                </div>
-                                <div className="cartstate">
-                                    <FaCircleMinus/><span>1</span><FaCirclePlus/>
-                                </div>
-                           </div>
-                           <div className="cartlist">
-                                <div className="cartimg">
-                                    <img src='tshirts.jpg' alt='tshirt'/>
-                                </div>
-                                <div className="carttext">
-                                    <span>
-                                    Wings Of Freedom Attack On Titan Tshirt (XL/Bottlegreen)
-                                    </span>
-                                </div>
-                                <div className="cartstate">
-                                    <FaCircleMinus/><span>1</span><FaCirclePlus/>
-                                </div>
-                           </div>
-                           <div className="cartlist">
-                                <div className="cartimg">
-                                    <img src='tshirts.jpg' alt='tshirt'/>
-                                </div>
-                                <div className="carttext">
-                                    <span>
-                                    Wings Of Freedom Attack On Titan Tshirt (XL/Bottlegreen)
-                                    </span>
-                                </div>
-                                <div className="cartstate">
-                                    <FaCircleMinus/><span>1</span><FaCirclePlus/>
-                                </div>
-                           </div>
-                           <div className="cartlist">
-                                <div className="cartimg">
-                                    <img src='tshirts.jpg' alt='tshirt'/>
-                                </div>
-                                <div className="carttext">
-                                    <span>
-                                    Wings Of Freedom Attack On Titan Tshirt (XL/Bottlegreen)
-                                    </span>
-                                </div>
-                                <div className="cartstate">
-                                    <FaCircleMinus/><span>1</span><FaCirclePlus/>
-                                </div>
-                           </div>
-                           
+    const toggleCartBar = () => {
+        const cartClassList = cartref.current.classList;
+
+        if (cartClassList.contains('top-[-110vh]')) {
+            cartClassList.remove('top-[-110vh]');
+            cartClassList.add('top-0');
+        } else {
+            // Remove the extra space in the class name
+            cartClassList.remove('top-0');
+            cartClassList.add('top-[-110vh]');
+        }
+    };
+
+    return (
+        <>
+            <div id='nav' className='p-2'>
+                <div className="logo-div">
+                    <Link className='cursor-pointer' href={'/'}><Image src='/logo.png' alt='img' height={250} width={150} /></Link>
+                </div>
+                <div ref={ref} className=" navList displaynone">
+                    <ul>
+                        <style jsx>
+                            {
+                                `
+                                .active{
+                                    border-bottom:2px solid black;
+                                }
+                                `
+                            }
+                        </style>
+                        <Link onClick={togglebar} className='cursor-pointer' href={'/tshirt'}><li className={pathname === '/tshirt' ? 'active' : ''}>Tshirts</li></Link>
+                        <Link onClick={togglebar} className='cursor-pointer' href={'/hoodies'}><li className={pathname === '/hoodies' ? 'active' : ''}>Hoodeis</li></Link>
+                        <Link onClick={togglebar} className='cursor-pointer' href={'/mugs'}><li className={pathname === '/mugs' ? 'active' : ''}>Mugs</li></Link>
+                        <Link onClick={togglebar} className='cursor-pointer' href={'/stickers'}><li className={pathname === '/stickers' ? 'active' : ''}>Stickers</li></Link>
+                        <Link onClick={togglebar} className='cursor-pointer' href={'/about'}><li className={pathname === '/about' ? 'active' : ''}>About</li></Link>
+                        <Link onClick={togglebar} className='cursor-pointer' href={'/contact'}><li className={pathname === '/contact' ? 'active' : ''}>Contact</li></Link>
+                        <Link onClick={togglebar} className='cursor-pointer' href={'/order'}><li className={pathname === '/order' ? 'active' : ''}>Order</li></Link>
+                        <li>  <FaShoppingCart onClick={toggleCartBar} className='cartopen' /></li>
+                    </ul>
+                </div>
+                <div onClick={togglebar} className='menubar'>
+                    {menuonoff ? <FaBars /> : <FaWindowClose />}
+                </div>
+            </div>
+            <div ref={cartref} className=" top-[-110vh] cartbar">
+                <div className="carthead">
+                    <h2>Shopping Cart</h2>
+                    <label htmlFor="carticon">
+
+                        <FaWindowClose onClick={toggleCartBar} className='cartclose' />
+                    </label>
+                </div>
+                <div className="cartitem">
+                    <div className="cartlist">
+                        <div className="cartimg">
+                            <Image width={200} height={200} src='/tshirts.jpg' alt='tshirt' />
                         </div>
-          <div className="cartbtn">
-              <Link  href={'/order'}><button className='bg-pink-500'><IoBagCheckOutline className='mr-1'/><span>Checkout</span></button></Link>
-          </div>
-      </div>
-    </>
-  )
+                        <div className="carttext">
+                            <span>
+                                Wings Of Freedom Attack On Titan Tshirt (XL/Bottlegreen)
+                            </span>
+                        </div>
+                        <div className="cartstate">
+                            <FaCircleMinus /><span>1</span><FaCirclePlus />
+                        </div>
+                    </div>
+                    <div className="cartlist">
+                        <div className="cartimg">
+                            <Image width={200} height={200} src='/tshirts.jpg' alt='tshirt' />
+                        </div>
+                        <div className="carttext">
+                            <span>
+                                Wings  (XL/Bottlegreen)
+                            </span>
+                        </div>
+                        <div className="cartstate">
+                            <FaCircleMinus /><span>1</span><FaCirclePlus />
+                        </div>
+                    </div>
+                    <div className="cartlist">
+                        <div className="cartimg">
+                            <Image width={200} height={200} src='/tshirts.jpg' alt='tshirt' />
+                        </div>
+                        <div className="carttext">
+                            <span>
+                                Wings Of Freedom Attack (XL/Bottlegreen)
+                            </span>
+                        </div>
+                        <div className="cartstate">
+                            <FaCircleMinus /><span>1</span><FaCirclePlus />
+                        </div>
+                    </div>
+                    <div className="cartlist">
+                        <div className="cartimg">
+                            <Image width={200} height={200} src='/tshirts.jpg' alt='tshirt' />
+                        </div>
+                        <div className="carttext">
+                            <span>
+                                Wings Of Freedom Attack (XL/Bottlegreen)
+                            </span>
+                        </div>
+                        <div className="cartstate">
+                            <FaCircleMinus /><span>1</span><FaCirclePlus />
+                        </div>
+                    </div>
+                    <div className="cartlist">
+                        <div className="cartimg">
+                            <Image width={200} height={200} src='/tshirts.jpg' alt='tshirt' />
+                        </div>
+                        <div className="carttext">
+                            <span>
+                                Wings Of Freedom Attack (XL/Bottlegreen)
+                            </span>
+                        </div>
+                        <div className="cartstate">
+                            <FaCircleMinus /><span>1</span><FaCirclePlus />
+                        </div>
+                    </div>
+                    <div className="cartlist">
+                        <div className="cartimg">
+                            <Image width={200} height={200} src='/tshirts.jpg' alt='tshirt' />
+                        </div>
+                        <div className="carttext">
+                            <span>
+                                Wings Of Freedom Attack (XL/Bottlegreen)
+                            </span>
+                        </div>
+                        <div className="cartstate">
+                            <FaCircleMinus /><span>1</span><FaCirclePlus />
+                        </div>
+                    </div>
+                    <div className="cartlist">
+                        <div className="cartimg">
+                            <Image width={200} height={200} src='/tshirts.jpg' alt='tshirt' />
+                        </div>
+                        <div className="carttext">
+                            <span>
+                                Wings Of Freedom Attack (XL/Bottlegreen)
+                            </span>
+                        </div>
+                        <div className="cartstate">
+                            <FaCircleMinus /><span>1</span><FaCirclePlus />
+                        </div>
+                    </div>
+                    <div className="cartlist">
+                        <div className="cartimg">
+                            <Image width={200} height={200} src='/tshirts.jpg' alt='tshirt' />
+                        </div>
+                        <div className="carttext">
+                            <span>
+                                Wings Of Freedom Attack (XL/Bottlegreen)
+                            </span>
+                        </div>
+                        <div className="cartstate">
+                            <FaCircleMinus /><span>1</span><FaCirclePlus />
+                        </div>
+                    </div>
+            {/* the last list is about cart buttons like checkout  */}
+                    <div className="cartbtn">
+                        <div className="carttotal">
+                            <span>Sub-total : 852 BDT</span>
+                        </div>
+                        <div className='btns'>
+                            <button className='bg-pink-500 mr-1 hover:bg-pink-600'><IoBagCheckOutline className='mr-1' /><span>Checkout</span></button>
+                            <button className='bg-pink-500 ml-1 hover:bg-pink-600'>Clear</button>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </>
+    )
 }
 
 export default Navbar
--- src/pages/stickers.js
+++ src/pages/stickers.js
@@ -1,8 +1,104 @@
 import React from 'react'
-
+import Image from 'next/image'
 const Stickers = () => {
   return (
-    <div>Stickers</div>
+    <>
+     <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-wrap -m-4">
+    <div className="lg:w-1/4 md:w-1/2 p-4 w-full tshirt-box">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <Image alt="tshirts" className="tshirtsphoto" src="/stickers.jpeg" width={596} height={600} />
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Tshirts</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">were the code</h2>
+          <p className="mt-1">BDT 399</p>
+          <p>size: sm , md, x , xl</p>
+        </div>
+      </div> 
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full tshirt-box">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <Image alt="tshirts" className="tshirtsphoto" src="/stickers.jpeg" width={596} height={600} />
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Tshirts</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">were the code</h2>
+          <p className="mt-1">BDT 399</p>
+          <p>size: sm , md, x , xl</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full tshirt-box">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <Image alt="tshirts" className="tshirtsphoto" src="/stickers.jpeg" width={596} height={600} />
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Tshirts</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">were the code</h2>
+          <p className="mt-1">BDT 399</p>
+          <p>size: sm , md, x , xl</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full tshirt-box">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <Image alt="tshirts" className="tshirtsphoto" src="/stickers.jpeg" width={596} height={600} />
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Tshirts</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">were the code</h2>
+          <p className="mt-1">BDT 399</p>
+          <p>size: sm , md, x , xl</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full tshirt-box">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <Image alt="tshirts" className="tshirtsphoto" src="/stickers.jpeg" width={596} height={600} />
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Tshirts</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">were the code</h2>
+          <p className="mt-1">BDT 399</p>
+          <p>size: sm , md, x , xl</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full tshirt-box">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <Image alt="tshirts" className="tshirtsphoto" src="/stickers.jpeg" width={596} height={600} />
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Tshirts</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">were the code</h2>
+          <p className="mt-1">BDT 399</p>
+          <p>size: sm , md, x , xl</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full tshirt-box">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <Image alt="tshirts" className="tshirtsphoto" src="/stickers.jpeg" width={596} height={600} />
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Tshirts</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">were the code</h2>
+          <p className="mt-1">BDT 399</p>
+          <p>size: sm , md, x , xl</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full tshirt-box">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <Image alt="tshirts" className="tshirtsphoto" src="/stickers.jpeg" width={596} height={600} />
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Tshirts</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">were the code</h2>
+          <p className="mt-1">BDT 399</p>
+          <p>size: sm , md, x , xl</p>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</section>
+    </>
   )
 }
 
