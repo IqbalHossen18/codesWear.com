cb1705dcb320521d206002926593943771ba3ca9
Iqbal <iqbalhossen1418@gmail.com>
2023-11-30 21:34:25 +0600

    after mongo code change

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -1,11 +1,24 @@
 import mongoose from "mongoose";
 
-const connectToMongo = handler=> async(req, res)=>{
-    if(mongoose.connections[0].readyState){
-        return handler(req, res)
+const connectToMongo = (handler) => async (req, res) => {
+  try {
+    // Check if there's already a connection
+    if (mongoose.connections[0].readyState) {
+      return handler(req, res);
     }
-    await mongoose.connect(process.env.MONGO_URI);
-    return handler(req, res)
-}
+
+    // If not, establish a new connection
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB"); // Add this line for logging
+
+    return handler(req, res);
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error.message);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 export default connectToMongo;
